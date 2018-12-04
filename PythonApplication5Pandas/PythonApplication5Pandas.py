14b894c34c308e46a1cf5b8a8b6ca6dc4c53cd7e
Blonska <shostak_ak15@nuwm.edu.ua>
2018-12-04 10:04:39 +0300

    Update PythonApplication5Pandas.py

--- PythonApplication5Pandas/PythonApplication5Pandas.py
+++ PythonApplication5Pandas/PythonApplication5Pandas.py
@@ -5,80 +5,13 @@ import copy
 
 class YieldEnergyComponent:
 
-    def _read_data(self,file_path):
-        read_data = pd.read_excel(file_path, sep=' ')
+    def _read_data(self,data):
+        read_data = pd.read_excel(data, sep=' ')
         if read_data.empty:
             raise Exception('Data is empty')
         return read_data
 
-
-    def _parcing_data(self,data,culture=None,param=None):
-        data_temp = copy.deepcopy(data)
-        if not culture==None:
-            data_temp = self.data1.loc[self.data1['Культура'] == culture]
-        if not param==None:
-            data_temp=self.data1.loc[self.data1['Параметр'].isin([param])]
-        return data_temp
-
-    def _parcing_data1(self,data,culture=None,param=None,border=None):
-        self.data1 = self._read_data(data)
-        if not culture==None:
-            self.data1= self.data1.loc[self.data1['Культура'] == culture]
-        if not param==None:
-            self.data1=self.data1.loc[self.data1['Параметр'].isin([param])]
-        if not border==None:
-            self.data1=self.data1.loc[self.data1['Межа'].isin([border])]
-        return self.data1
-      
-
-    def _tay(self,Di=None,Mi=None):
-        if Di==None:
-            Di=self.data1.loc[self.data1['Параметр'] == 'Di']
-        if Mi==None:
-            Mi=self.data1.loc[self.data1['Параметр'] == 'Mi']
-        '''tay - number of days from the start of the growing season
-           Mi - numders month the end of the growing season
-           Di - the day of the month the end of the growing season'''
-
-        tay = Di + 30.5 * (Mi - 5) + 10.5
-        return tay
-
-
-    def _xt1(self,Di,Mi):
-        '''
-        xt1 - the average multi-year sum of active air temperatures for vegetation(Polissya)
-        '''
-
-        xt1 = 2829.4 / (1 + 7.7 * np.exp(-0.029 * self._tay(Di,Mi) - 1.62))
-        return xt1
-
-
-    def _xt2(self,Di,Mi):
-        '''
-        xt2 - the average multi-year sum of active air temperatures for vegetation(Steppe and Forest-steppe)
-        '''
-
-        xt2 = 2712.7 / (1 + 72.9 * np.exp(-0.03 * self._tay(Di,Mi) - 1.61))
-        return xt2
-
-
-    def YT(self,yb,yot,kt,lt,mt,Mi,Di,region):
-        '''yt - energy component of performance
-           yb - maximum productivity of culture
-           yot,kt,lt,mt - model parameters'''
-       
-        region = lambda x: self._xt1(Di,Mi) if _xt1=='Полісся' else self._xt2(Di,Mi)
-        yt = (yb * yot * np.exp(-kt * ((region - lt) ** 2))) / (1 + (100 - yot) * np.exp(-mt * (region - lt)))
-        return yt
-
-class YieldSoilComponent:
-
-    def _read_data(self,file_path):
-        read_data = pd.read_excel(file_path, sep=' ')
-        if read_data.empty:
-            raise Exception('Data is empty')
-        return read_data
-
+    #ТаблицяБ11,ТаблицяБ21,
     def _parcing_data(self,data,culture=None,param=None,border=None):
         self.data1 = self._read_data(data)
         if not culture==None:
@@ -88,20 +21,70 @@ class YieldSoilComponent:
         if not border==None:
             self.data1=self.data1.loc[self.data1['Межа'].isin([border])]
         return self.data1
+    
+    def _tay(self,data=None,Di=None,Mi=None,culture=None,border=None):
+        if Di==None:
+            Di=self._parcing_data(data,culture,'Di',border)
+        if Mi==None:
+            Mi=self._parcing_data(data,culture,'Mi',border)
+        '''tay - number of days from the start of the growing season
+           Mi - numders month the end of the growing season
+           Di - the day of the month the end of the growing season'''
 
+        tay = Di.iat[0,3] + 30.5 * ((Mi.iat[0,3]) - 5) + 10.5
+        return tay
+
+
+    def _xt1(self,data=None,Di=None,Mi=None,culture=None,border=None):
+        '''
+        xt1 - the average multi-year sum of active air temperatures for vegetation(Polissya)
+        '''
+
+        xt1 = 2829.4 / (1 + 7.7 * np.exp(-0.029 * self._tay(data,Di,Mi,culture,border) - 1.62))
+        return xt1
+
+
+    def _xt2(self,data=None,Di=None,Mi=None,culture=None,border=None):
+        '''
+        xt2 - the average multi-year sum of active air temperatures for vegetation(Steppe and Forest-steppe)
+        '''
+
+        xt2 = 2712.7 / (1 + 72.9 * np.exp(-0.03 * self._tay(data,Di,Mi,culture,border) - 1.61))
+        return xt2
+
+
+    def YT(self,data=None,Mi=None,Di=None,culture=None,border=None,yb=None,yot=None,kt=None,lt=None,mt=None):
+        '''yt - energy component of performance
+           yb - maximum productivity of culture
+           yot,kt,lt,mt - model parameters'''
+        if yb==None:
+            yb=self._parcing_data(data,culture,'yb',border)
+        if yot==None:
+            yot=self._parcing_data(data,culture,'yot',border)
+        if lt==None:
+            lt=self._parcing_data(data,culture,'lt',border)
+        if mt==None:
+            mt=self._parcing_data(data,culture,'mt',border)
+        region = lambda x: self._xt1(data,Di,Mi,culture,border) if _xt1=='Полісся' else self._xt2(data,Di,Mi,culture,border)
+        yt = (yb * yot * np.exp(-kt * ((region - lt) ** 2))) / (1 + (100 - yot) * np.exp(-mt * (region - lt)))
+        return yt
+
+class YieldSoilComponent:
+
+    #ТаблицяБ41
     def _parcing_data1(self,data,culture=None,soilparam=None,param=None):
-        self.data1 = self._read_data(data)
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not culture==None:
-            self.data1= self.data1.loc[self.data1['Культура'] == culture]
+            self.data1 = self.data1.loc[self.data1['Культура'] == culture]
         if not soilparam==None:
-            self.data1=self.data1.loc[self.data1['Грунтовий показник'].isin([soilparam])]
+            self.data1 = self.data1.loc[self.data1['Грунтовий показник'].isin([soilparam])]
         if not param==None:
-            self.data1=self.data1.loc[self.data1['Параметр'].isin([param])]
+            self.data1 =self.data1.loc[self.data1['Параметр'].isin([param])]
         return self.data1
 
-
+    #ТаблицяБ5
     def _parcing_data2(self,data,zone=None,soil_type=None,granulometric_composition=None,border=None):
-        self.data1 = self._read_data(data)
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not zone==None:
             self.data1= self.data1.loc[self.data1['Зона'] == zone]
         if not soil_type==None:
@@ -112,8 +95,9 @@ class YieldSoilComponent:
             self.data1=self.data1.loc[self.data1['Межа'].isin([border]) ]
         return self.data1
 
+    #ТаблицяБ61
     def _parcing_data3(self,data,soilparam=None,param1=None,param2=None,param3=None,param4=None,param5=None,hama1=None):
-        self.data1 = self._read_data(data)
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not soilparam==None:
             self.data1= self.data1.loc[self.data1['Грунтовий показник'] == soilparam]
         if not param1==None:
@@ -130,16 +114,17 @@ class YieldSoilComponent:
             self.data1=self.data1.loc[self.data1['Гама1'].isin([hama1]) ]
         return self.data1
 
+    #ТаблицяБ62
     def _parcing_data4(self,data,soilparam=None,G=None,Eh=None,d=None,H=None,P=None,K=None,pH=None,h=None,W=None,hama2=None):
-        self.data1 = self._read_data(data)
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not soilparam==None:
             self.data1= self.data1.loc[self.data1['Грунтовий показник'] == soilparam]
         if not G==None:
-            self.data1=self.data1.loc[self.data1['G(Z)'].isin([G])]
+            self.data1=self.data1.loc[self.data1['G'].isin([G])]
         if not Eh==None:
             self.data1=self.data1.loc[self.data1['Eh'].isin([Eh])]
         if not d==None:
-            self.data1=self.data1.loc[self.data1['d'].isin([d])]
+            self.data1=self.data1.loc[self.data1['D'].isin([d])]
         if not H==None:
             self.data1=self.data1.loc[self.data1['H'].isin([H])]
         if not P==None:
@@ -156,8 +141,9 @@ class YieldSoilComponent:
             self.data1=self.data1.loc[self.data1['Гама2'].isin([hama2])]
         return self.data1
 
-    def _parcing_data5ideal(self,data,param=None,diraction=None,mark1=None,medaction=None,mark2=None,hama3=None):
-         self.data1 = self._read_data(data)
+    #ТаблицяБ6
+    def _parcing_data5(self,data,param=None,diraction=None,mark1=None,medaction=None,mark2=None,hama3=None):
+         self.data1 = YieldEnergyComponent._read_data(self,data)
          if not param==None:
              self.data1= self.data1.loc[self.data1['Показник'] == param]
          if not diraction==None:
@@ -169,7 +155,7 @@ class YieldSoilComponent:
          if not mark2==None:
              self.data1=self.data1.loc[self.data1['Бали2'].isin([mark2])]
          if not hama3==None:
-             self.data1=self.data1.loc[self.data1['Вага Гама3'].isin([hama3])]
+             self.data1=self.data1.loc[self.data1['Гама3'].isin([hama3])]
          return self.data1
     
     def _hama(hama1=None,hama2=None,hama3=None):
@@ -178,7 +164,7 @@ class YieldSoilComponent:
         if hama2==None:
             hama2=self.data1.loc[self.data1['Гама2'] == hama2]
         if hama3==None:
-            hama3=self.data1.loc[self.data1['Вага Гама3'] == hama2]
+            hama3=self.data1.loc[self.data1['Гама3'] == hama2]
         hama=1 / 3 * (hama1 + hama2 + hama3)
         return hama
 
@@ -199,43 +185,48 @@ class YieldSoilComponent:
            yop,kp,lp,mp - parameters of the model of tolerance
            xp - meaning of soil indicators
         '''
+        if yb==None:
+            yb=YieldEnergyComponent._parcing_data(data,culture,'yb',border)
+        if yop==None:
+            yop=self._parcing_data1(data,culture,soilparam,'yop')
+        if kp==None:
+            lt=self._parcing_data1(data,culture,soilparam,'kp')
+        if lp==None:
+            lp=self._parcing_data(data,culture,soilparam,'lp')
+        #xp(_parcing_data2)
         yp = hama * (nf * nl * ns * nd * kf * kwt * ks * kl * kd * ki * kh * kw * yb * yop * np.exp(-kp * ((xp - lp) ** 2))) / (1 + (100 - yop) * np.exp(-mp * (xp - lp)))
         return yp
 
 class WeighSoilComponent:
-    def _read_data(self,file_path):
-        read_data = pd.read_excel(file_path, sep=' ')
-        if read_data.empty:
-            raise Exception('Data is empty')
-        return read_data
 
-    def _parcing_data(self,data,param=None,alfa1=None,alfa2=None,alfa3=None,mark1=None,param1=None,param2=None,mark2=None):
-        self.data1 = self._read_data(data)
+    #ТаблицяБ7
+    def _parcing_data(self,data,param=None,alfa1=None,alfa2=None,param1=None,mark1=None,param2=None,mark2=None,alfa3=None):
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not param==None:
             self.data1= self.data1.loc[self.data1['Параметр продуктивності '] == param]
         if not alfa1==None:
             self.data1=self.data1.loc[self.data1['Alfa f1'].isin([alfa1])]
         if not alfa2==None:
             self.data1=self.data1.loc[self.data1['Alfa f2'].isin([alfa2])]
-        if not alfa3==None:
-            self.data1=self.data1.loc[self.data1['Alfa f3'].isin([alfa3])]
+        if not param1==None:
+            self.data1=self.data1.loc[self.data1['Заходи безпосереднього впливу'].isin([alfa3])]
         if not mark1==None:
             self.data1=self.data1.loc[self.data1['Бали1'].isin([mark1])]
-        if not param1==None:
-            self.data1=self.data1.loc[self.data1['Заходи безпосереднього впливу'].isin([param1])]
         if not param2==None:
-            self.data1=self.data1.loc[self.data1['Заходи опосередкованого впливу '].isin([param2])]
+            self.data1=self.data1.loc[self.data1['Заходи опосередкованого впливу'].isin([param1])]
         if not mark2==None:
-            self.data1=self.data1.loc[self.data1['Бали2'].isin([Alfa3])]
+            self.data1=self.data1.loc[self.data1['Бали2'].isin([param2])]
+        if not alfa3==None:
+            self.data1=self.data1.loc[self.data1['Alfa f3'].isin([Alfa3])]
         return self.data1
 
-    def _alfa(alfa1=None,alfa2=None,alfa3=None):
+    def _alfa(self,data=None,alfa1=None,alfa2=None,alfa3=None):
         if alfa1==None:
-            alfa1=self.data1.loc[self.data1['Alfa f1'] == alfa1]
+            alfa1=self.data1[self.data1['Alfa f1'] == alfa1]
         if alfa2==None:
-            alfa2=self.data1.loc[self.data1['Alfa f2'] == alfa2]
+            alfa2=self.data1[self.data1['Alfa f2'] == alfa2]
         if alfa3==None:
-            alfa3=self.data1.loc[self.data1['Alfa f3'] == alfa3]
+            alfa3=self.data1[self.data1['Alfa f3'] == alfa3]
         alfa=1 / 3 * (alfa1 + alfa2 + alfa3)
         return alfa
 
@@ -244,14 +235,9 @@ class WeighSoilComponent:
          return yp
 
 class CumulativeSoilComponent:
-     def _read_data(self,file_path):
-        read_data = pd.read_excel(file_path, sep=' ')
-        if read_data.empty:
-            raise Exception('Data is empty')
-        return read_data
-
-     def _parcing_data(self,data,param=None,units=None,value=None,typeofsoil=None):
-        self.data1 = self._read_data(data)
+     #ТаблицяБ8
+     def _parcing_data(self,data,param=None,units=None,value=None,typeofsoil=None,xij=None,xij2=None):
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not param==None:
             self.data1= self.data1.loc[self.data1['Показник'] == param]
         if not units==None:
@@ -262,7 +248,7 @@ class CumulativeSoilComponent:
             self.data1=self.data1.loc[self.data1['Тип грунту'].isin([typeofsoil])]
         return self.data1
 
-     def _G(self,xij=None,xij2=None):
+     def _q(self,xij=None,xij2=None):
          if xij==None:
             xij=self.data1.loc[self.data1['xij'] == xij]
          if xij2==None:
@@ -273,18 +259,13 @@ class CumulativeSoilComponent:
              return 0
 
      def YS(self,nf,nl,ns,nd,kf,kwt,kl,kd,ki,kh,kw,yb,q):
-         ys = (nf * nl * ns * nd * kf * kwt * kl * kd * ki * kh * kw * yb * 0.08 * np.exp(-1.66 * (q - 0.24) ^ 2))/(0.01 + 0.92 * np.exp(-4.992 * (q - 0.24)))
+         ys = (nf * nl * ns * nd * kf * kwt * kl * kd * ki * kh * kw * yb * 0.08 * np.exp(-1.66 * (self._q(xij,xij2)- 0.24) ^ 2))/(0.01 + 0.92 * np.exp(-4.992 * (self._q(xij,xij2) - 0.24)))
          return ys
 
 class Productivityofland:
-     def _read_data(self,file_path):
-        read_data = pd.read_excel(file_path, sep=' ')
-        if read_data.empty:
-            raise Exception('Data is empty')
-        return read_data
-
+     #ТаблицяБ9
      def _parcing_data(self,data,culture=None,param=None,value=None):
-        self.data1 = self._read_data(data)
+        self.data1 = YieldEnergyComponent._read_data(self,data)
         if not culture==None:
             self.data1= self.data1.loc[self.data1['Культура'] == culture]
         if not param==None:
@@ -293,7 +274,11 @@ class Productivityofland:
             self.data1=self.data1.loc[self.data1['Значення параметра'].isin([value])]
         return self.data1
 
-     def YE(self,Cs,Cl,yl,ys):
+     def YE(self,data,Cs=None,Cl=None,yl=None,ys=None,culture=None,value=None):
+        if Cs==None:
+            yop=self._parcing_data(data,culture,'Cs',value)
+        if Cs==None:
+            yop=self._parcing_data(data,culture,'Cl',value)
         ye = Cs * ys + Cl * yl
         return ye
 
@@ -303,7 +288,7 @@ class Productivityofland:
     
 
 object = Productivityofland()
-print(object._parcing_data(data='D:\ТаблицяБ9.xlsx',culture='Озима пшениця',param='Cs'))
+print(object.YE(data='D:\ТаблицяБ9.xlsx',culture='Овес',ys=3,yl=8))
 
 #if __name__ == "__main__":
     #yield_soil_component = YieldSoilComponent()
